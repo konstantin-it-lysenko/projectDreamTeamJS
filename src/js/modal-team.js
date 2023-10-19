bf36b4603ea74434d2816466ce510d00d6288dea
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2023-10-19 22:08:01 +0300

    team-modal start

---
+++ src/js/modal-team.js
@@ -0,0 +1,14 @@
+const teamModalOpenBtn = document.querySelector('.team-btn-open');
+const teamModalCloseBtn = document.querySelector('.team-btn-close');
+const teamModal = document.querySelector('.team-backdrop');
+
+teamModalOpenBtn.addEventListener('click', onOpenClick);
+teamModalCloseBtn.addEventListener('click', onCloseClick);
+
+function onOpenClick() {
+  teamModal.classList.remove('is-hidden');
+}
+
+function onCloseClick() {
+  teamModal.classList.add('is-hidden');
+}
