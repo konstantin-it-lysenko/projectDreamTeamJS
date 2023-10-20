c2a59041072e6323cf1c31aba24c95bfffbc2aa3
Denys-Nalyvaiko <nalyvaikodenys@gmail.com>
2023-10-20 15:47:23 +0200

    feature/fetch-modal-exercise

--- src/js/modal-exercise.js
+++ src/js/modal-exercise.js
@@ -1,6 +1,6 @@
 import * as basicLightbox from 'basiclightbox';
-// import 'basicLightbox/dist/basicLightbox.min.css';
 import '../../node_modules/basiclightbox/dist/basicLightbox.min.css';
+import { fetchExerciseModalById } from './api-service/modal-exercise-api';
 import { createModalExerciseMarkup } from './templates/modal-exercise-markup';
 
 const CLOSE_KEY = 'Escape';
@@ -11,19 +11,24 @@ const openModalExerciseBtnRef = document.querySelector(
 
 openModalExerciseBtnRef.addEventListener('click', handleOpenModalClick);
 
-function handleOpenModalClick() {
-  const instance = basicLightbox.create(createModalExerciseMarkup(), {
-    onShow: instance => {
-      instance
-        .element()
-        .querySelector('[data-modal-exercise="close"]').onclick =
-        instance.close;
-      window.addEventListener('keydown', handleCloseModalKeyDown);
-    },
-    onClose: instance => {
-      window.removeEventListener('keydown', handleCloseModalKeyDown);
-    },
-  });
+async function handleOpenModalClick() {
+  const exericiseData = await fetchExerciseModalById();
+
+  const instance = basicLightbox.create(
+    createModalExerciseMarkup(exericiseData),
+    {
+      onShow: instance => {
+        instance
+          .element()
+          .querySelector('[data-modal-exercise="close"]').onclick =
+          instance.close;
+        window.addEventListener('keydown', handleCloseModalKeyDown);
+      },
+      onClose: instance => {
+        window.removeEventListener('keydown', handleCloseModalKeyDown);
+      },
+    }
+  );
 
   function handleCloseModalKeyDown(event) {
     if (event.code === CLOSE_KEY) {
