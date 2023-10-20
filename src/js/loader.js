8bc7a6c2593bd4ecd1b108a73b3ac952e5617248
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2023-10-20 12:16:08 +0300

    повторно поки без мін

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,5 +1,5 @@
 let mask = document.querySelector(".mask");
-
+console.log(mask);
 window.addEventListener("load", makesDisappear);
 
 
@@ -9,3 +9,19 @@ function makesDisappear () {
     setTimeout(() => {mask.remove();
     }, 600);
 }
+// let mask = document.querySelector(".mask");
+
+// export function name(params) {
+
+// }
+// export function name(params) {
+    
+// }
+
+// window.addEventListener("load", makesDisappear);
+
+// function makesDisappear () {
+//     mask.classList.add("hide");
+//     setTimeout(() => {mask.remove();
+//     }, 600);
+// }
