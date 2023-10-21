9cdc861a462800d85459e06e4737d00a2c1e7217
IhorPozhematkin <132217496+IhorPozhematkin@users.noreply.github.com>
2023-10-21 11:47:01 +0300

    sidebar-21.10-1

--- src/js/quote.js
+++ src/js/quote.js
@@ -4,37 +4,25 @@ import Notiflix from 'notiflix';
 
 const { BASE_URL, QUOTE_ENDPOINT } = API_PROPS;
 
-async function fetchQuote() {
+async function fetchAndDisplayQuote() {
   try {
     const response = await axios.get(`${BASE_URL}${QUOTE_ENDPOINT}`);
-    return response.data;
-  } catch {
-    Notiflix.Notify.failure('Please try again.');
-    return {};
-  }
-}
-
-async function updateQuote() {
-  const today = new Date();
-  const savedDate = localStorage.getItem('quoteDate');
-  if (savedDate !== today) {
-    const data = await fetchQuote();
-    if (data) {
+    const data = response.data;
+    if (data && data.quote && data.author) {
+      const today = new Date();
       localStorage.setItem('quote', data.quote);
       localStorage.setItem('author', data.author);
       localStorage.setItem('quoteDate', today);
-      return data;
+      const quoteElement = document.querySelector('.quote');
+      const authorElement = document.querySelector('.author');
+      quoteElement.textContent = data.quote;
+      authorElement.textContent = data.author;
+    } else {
+      Notiflix.Notify.failure('Please try again.');
     }
+  } catch (error) {
+    Notiflix.Notify.failure('Please try again.');
   }
-  return {};
 }
 
-async function displayInfo() {
-  const quoteData = await updateQuote();
-  const quoteElement = document.querySelector('.quote');
-  const authorElement = document.querySelector('.author');
-  quoteElement.textContent = quoteData.quote;
-  authorElement.textContent = quoteData.author;
-}
-
-displayInfo();
+fetchAndDisplayQuote();
