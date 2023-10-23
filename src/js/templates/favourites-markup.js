9ac22ecf054d277096cbb06d7485e5c772874c57
ArtanFS <132677965+ArtanFS@users.noreply.github.com>
2023-10-23 21:32:23 +0300

    ex cards btn

--- src/js/templates/favourites-markup.js
+++ src/js/templates/favourites-markup.js
@@ -1,16 +1,20 @@
 function createMarkupExercises(arr) {
   return arr
     .map(
-      ({ name, burnedCalories, bodyPart, target }) =>
-        `<li><div class="favor-exercises-card">
+      ({ _id, name, burnedCalories, bodyPart, target }) =>
+        `<li><div class="favor-exercises-card" id="${_id}">
       <div class="favor-exercises-head">
-        <div class="favor-workout"><p>Workout</p></div>
-        <button type="button">
-          <svg width="16" height="16">
-            <use href="./img/sport-sprite.svg#icon-trash"></use>
-          </svg>
-        </button>
-        <button type="button">Start</button>
+        <div class="favor-exercises-wrap">
+          <div class="favor-workout"><p>Workout</p></div>
+          <button class="favor-exercises-delbtn" type="button">
+            <svg width="16" height="16">
+              <use href="./img/sport-sprite.svg#icon-trash"></use>
+            </svg>
+          </button>
+        </div>
+        <button class="favor-exercises-startbtn" type="button">Start<svg width="16" height="16">
+            <use href="./img/sport-sprite.svg#icon-arrow-up"></use>
+          </svg></button>
       </div>
       <div class="favor-exercises-name">
         <div class="favor-exercises-icon">
@@ -22,10 +26,10 @@ function createMarkupExercises(arr) {
       </div>
       <ul class="favor-exercises-info">
         <li class="favor-exercises-item favor-exercises-calories">
-          <span>Burned calories:</span>${burnedCalories} / 3 min
+          Burned calories:<span>${burnedCalories} / 3 min</span>
         </li>
-        <li class="favor-exercises-item favor-exercises-body"><span>Body part:</span>${bodyPart}</li>
-        <li class="favor-exercises-item favor-exercises-target"><span>Target:</span>${target}</li>
+        <li class="favor-exercises-item favor-exercises-body">Body part:<span class="favor-exercises-span">${bodyPart}</span></li>
+        <li class="favor-exercises-item favor-exercises-target">Target:<span class="favor-exercises-span">${target}</span></li>
       </ul>
     </div></li>`
     )
