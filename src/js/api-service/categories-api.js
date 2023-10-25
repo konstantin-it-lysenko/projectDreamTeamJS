7ed9bfb7b27870022187455543ab531aa8ad2dc5
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2023-10-25 13:38:26 +0300

    update limit per page in api requests

--- src/js/api-service/categories-api.js
+++ src/js/api-service/categories-api.js
@@ -3,8 +3,7 @@ import { API_PROPS } from '../api/api';
 
 const { BASE_URL, FILTERS_ENDPOINT } = API_PROPS;
 
-// let limit = window.innerWidth < 768 ? 9 : 12;
-let limit = window.innerWidth < 768 ? 3 : 5;
+let limit = window.innerWidth < 768 ? 9 : 12;
 
 export async function fetchCategories(category = 'Body parts', page = 1) {
   const params = new URLSearchParams({
